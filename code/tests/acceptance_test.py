b146ff75c72a5fcc070c1d2d3fdbf42c4e39fe38
akaldemir <kgn.aldmr@gmail.com>
2023-08-06 10:22:22 +0200

    Added acceptance test

---
+++ code/tests/acceptance_test.py
@@ -0,0 +1,16 @@
+import json
+
+from main import main
+
+
+def test_acceptance():
+    main("test_data/pricat.csv",
+         "test_data/mappings.csv",
+         "test_data/test_catalog.json"
+         )
+    with open("test_data/test_catalog.json", "r") as catalog_test_file:
+        test_catalog_json = json.load(catalog_test_file)
+        with open("test_data/final_catalog.json", "r") as catalog_file:
+            catalog_json = json.load(catalog_file)
+
+            assert catalog_json == test_catalog_json
